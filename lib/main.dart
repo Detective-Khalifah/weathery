a18df40fa8e0e9f53ff5547264c4949214f124aa
Detective_Khalifah <detective.khalifah.dtcfscd@gmail.com>
2021-07-03 23:23:39 +0100

    Seek permission and get location data.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext contet) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Weathery App',
       theme: ThemeData(
         // This is the theme of the Weathery application.
@@ -39,6 +41,72 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late double latitude = 0, longitude = 0;
+  late bool permissionGranted;
+
+  /// Determine the current position of the device.
+  ///
+  /// When the location services are not enabled or permissions
+  /// are denied the `Future` will return an error.
+  Future<Position> _getUserDeviceLocation() async {
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    Position pos = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.low);
+    setState(() {
+      latitude = pos.latitude;
+      longitude = pos.longitude;
+    });
+    return pos;
+  }
+
+  Future _getLocationAccess() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+
+    switch (permission) {
+      case LocationPermission.denied:
+        // TODO: Handle this case.
+        permission = await Geolocator.requestPermission();
+        if (permission == LocationPermission.denied) {
+          // Permissions are denied, next time you could try
+          // requesting permissions again (this is also where
+          // Android's shouldShowRequestPermissionRationale
+          // returned true. According to Android guidelines
+          // your App should show an explanatory UI now.
+          permissionGranted = false;
+          return Future.error('Location permissions are denied');
+        }
+        break;
+      case LocationPermission.deniedForever:
+        // TODO: Permissions are denied forever, handle appropriately.
+        permissionGranted = false;
+        return Future.error(
+            'Location permissions are permanently denied, we cannot request permissions.');
+      case LocationPermission.whileInUse:
+      case LocationPermission.always:
+        permissionGranted = true;
+        break;
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getLocationAccess().whenComplete(() => _getUserDeviceLocation());
+  }
+
   @override
   Widget build(BuildContext context) {
     // The Flutter framework has been optimized to make rerunning build methods
@@ -56,8 +124,11 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'First route of app.',
+            ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                'Device Location:: Latitude: $latitude, Longitude: $longitude',
+              ),
             ),
           ],
         ),
